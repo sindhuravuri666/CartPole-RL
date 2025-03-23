fa2055fc9a59887ae44bff447c099510f4e6b285
Sindhu Ravuri <sindhuravuri666@gmail.com>
2025-03-23 17:09:30 +0530

    State

--- Cartpole.py
+++ Cartpole.py
@@ -1,13 +1,26 @@
 import gym
 import time
+import matplotlib 
+import random
+import numpy as np
 
 
 env = gym.make("CartPole-v1", render_mode="human")
+print("Onservational Space:",env.observation_space)
+print("Action Space:",env.action_space)
+
+print("Observation Space High:",env.observation_space.high)
+print("Observation Space Low:",env.observation_space.low)
+
+
+
 
 num_episodes = 5  # Run for 5 episodes
 
 for episode in range(num_episodes):
     state = env.reset()
+    action = env.action_space.sample()  # Random action
+    env.step(action)
     done = False
     score = 0  
 
